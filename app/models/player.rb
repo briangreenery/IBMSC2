4b4d995ea7abd475fc57bb08bd24e1a729d6be28
Brian Green <briangreenery@gmail.com>
2012-02-03 23:27:35 -0800

    Add a validation for the Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,6 +3,10 @@ class Player < ActiveRecord::Base
   has_many :games_won, :class_name => "Match", :foreign_key => "winner_id", :dependent => :delete_all
   has_many :games_lost, :class_name => "Match", :foreign_key => "loser_id", :dependent => :delete_all
 
+  validates :name, :presence => true
+  validates_format_of :character_code, :with => /^$|^[0-9]{2,3}$/, :message => "must be 3 or 4 digits long"
+  validates :points, :presence => true, :numericality => { :only_integer => true }
+
   def name_with_code
     if character_code.nil?
       name
