873e83aa921e8d1027ea3142c8fb012e0f7d2f3b
Brian Green <briangreenery@gmail.com>
2012-02-15 02:45:28 -0800

    Sort maps by name. Remove weird link hover

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,7 +2,6 @@ class ResultsController < ApplicationController
   def index
   	@players = Player.all( :order => 'points DESC' )
   	@matches = Match.all( :order => 'time DESC, id DESC' )
-  	@maps = Map.all
+  	@maps = Map.all( :order => "lower( name )" )
   end
-
 end
