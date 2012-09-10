4d994a19b11dccb681d5abd9aaeefc4ccfc3d2b9
Brian Green <briangreenery@gmail.com>
2012-09-09 17:40:45 -0700

    Improve the page load time of the main page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,51 @@
 class ResultsController < ApplicationController
   def index
-  	@players = Tournament.current.players
-  	@matches = Tournament.current.matches
-  	@maps = Map.all( :order => "lower( name )" )
+    tournament = Tournament.current
+
+    @matches = tournament.matches
+
+    @wins = {}
+    @wins.default = 0
+    @losses = {}
+    @losses.default = 0
+
+    @matches.each do |match|
+      @wins[match.winner_id] += 1
+      @losses[match.loser_id] += 1
+    end
+
+    @players = []
+    @points = {}
+    @ranks = {}
+
+    last_points = -1
+    rank = 0
+    rank_count = 1
+
+    Standing.where( :tournament_id => tournament.id ).order( 'points DESC' ).each do |standing|
+      if @wins[standing.player_id] > 0 || @losses[standing.player_id] > 0
+
+        @players.push standing.player_id
+        @points[standing.player_id] = standing.points
+
+        if standing.points != last_points
+          last_points = standing.points
+          rank += rank_count
+          rank_count = 0
+        end
+
+        rank_count += 1
+        @ranks[standing.player_id] = rank
+      end
+    end
+
+    @names = {}
+    @leagues = {}
+
+    Player.all.each do |player|
+      @names[player.id] = player.name
+      @leagues[player.id] = player.league_name
+    end
+
   end
 end
